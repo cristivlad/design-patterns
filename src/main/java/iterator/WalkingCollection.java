3629b0c2a0c7630f385ccc300516b85de9cdc5ba
CristiV <cristian.vlad10@gmail.com>
2024-01-22 19:52:08 +0200

    fix iterator

--- src/main/java/iterator/WalkingCollection.java
+++ src/main/java/iterator/WalkingCollection.java
@@ -3,10 +3,12 @@ package iterator;
 import java.util.AbstractCollection;
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class WalkingCollection<E>
         extends AbstractCollection<E> {
     private final Collection<E> wrappedCollection;
+    private ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
 
     public WalkingCollection(Collection<E> wrappedCollection) {
         this.wrappedCollection = wrappedCollection;
@@ -15,7 +17,16 @@ public class WalkingCollection<E>
     public void iterate(Processor<? super E> processor) {
         // lock using a ReadLock, then iterate through collection calling
         // processor.process(e) on each element
-        throw new UnsupportedOperationException("please implement");
+        lock.readLock().lock();
+        try {
+            for (E e : wrappedCollection) {
+                if (!processor.process(e))
+                    return;
+            }
+        } finally {
+            lock.readLock().unlock();
+        }
+//        throw new UnsupportedOperationException("please implement");
     }
 
     public Iterator<E> iterator() {
@@ -27,17 +38,75 @@ public class WalkingCollection<E>
 
         // Should throw IllegalMonitorStateException if a thread tries to call
         // remove() during iteration.
-        throw new UnsupportedOperationException("please implement");
+        lock.readLock().lock();
+        try {
+            Iterator<E> iterator = wrappedCollection.iterator();
+            return new Iterator<>() {
+
+                @Override
+                public boolean hasNext() {
+                    lock.readLock().lock();
+                    try {
+                        return iterator.hasNext();
+                    } finally {
+                        lock.readLock().unlock();
+                    }
+                }
+
+                @Override
+                public E next() {
+                    lock.readLock().lock();
+                    try {
+                        return iterator.next();
+                    } finally {
+                        lock.readLock().unlock();
+                    }
+                }
+
+                @Override
+                public void remove() {
+                    checkReadLockStatus();
+                    lock.writeLock().lock();
+                    try {
+                        iterator.remove();
+                    } finally {
+                        lock.writeLock().unlock();
+                    }
+                }
+            };
+        } finally {
+            lock.readLock().unlock();
+        }
+//        throw new UnsupportedOperationException("please implement");
+    }
+
+    private void checkReadLockStatus() {
+        if
+        (lock.getReadHoldCount() > 0)
+            throw new IllegalMonitorStateException("ReadLock is held");
     }
 
     public int size() {
         // the size of the wrappedCollection, but wrapped with a ReadLock
-        throw new UnsupportedOperationException("please implement");
+        lock.readLock().lock();
+        try {
+            return wrappedCollection.size();
+        } finally {
+            lock.readLock().unlock();
+        }
+//        throw new UnsupportedOperationException("please implement");
     }
 
     public boolean add(E e) {
         // adds the element to the collection, throws IllegalMonitorStateException
         // if that thread is busy iterating
-        throw new UnsupportedOperationException("please implement");
+        checkReadLockStatus();
+        lock.writeLock().lock();
+        try {
+            return wrappedCollection.add(e);
+        } finally {
+            lock.writeLock().unlock();
+        }
+//        throw new UnsupportedOperationException("please implement");
     }
 }
